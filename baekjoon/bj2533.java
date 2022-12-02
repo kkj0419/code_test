ba55597ce4b29ef763a4acb0b03ddb182c7bcf13
Hyeonjeong Kim <72663337+kkj0419@users.noreply.github.com>
2022-12-02 16:03:32 +0900

    [2022.12.02] 2533 SNS
    임포트문 정리

--- baekjoon/bj2533.java
+++ baekjoon/bj2533.java
@@ -2,10 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.stream.Stream;
 
 public class Main {
 
